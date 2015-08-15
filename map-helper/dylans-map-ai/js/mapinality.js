f8e4d8374b5497c3785c3e1223d4f496f7710f9e
thenewkid <cyberkid108@gmail.com>
2015-08-15 09:55:21 -0700

    Prototype 1

--- map-helper/dylans-map-ai/js/mapinality.js
+++ map-helper/dylans-map-ai/js/mapinality.js
@@ -1,61 +1,163 @@
 
+function addMapLoadListener() {
+	google.maps.event.addDomListener(window, 'load', initMap);
+}
 function initMap() {
+
 	var markers = [];
 	var map;
 	var bounds;
 	var currentPlaces = [];
+	var service;
+	var infoWindow;
+	var mapOptions;
+	var googleSearchBox;
+	var divsAndPlaceIds = {}
 
-	function makeNewDivElement(placeObject) {
+	function getAdditionalInfo(pid) {
+
+		function showReviewsDataInModal(reviewObjects, name) {
+
+			$("#modal-place-name").html(name);
+
+			reviewObjects.forEach(function(ro) {
+				var reviewDiv = document.createElement("div");
+				reviewDiv.className = "modal-review-div";
+
+				reviewDiv.innerHTML += "<h3 class='text-center'>By: " + ro.author_name + "</h3>";
+				reviewDiv.innerHTML += "<h3>Rating: " + ro.rating + "</h3>";
+				reviewDiv.innerHTML += "<h3>FeedBack: " + ro.text + "</h3>";
+				reviewDiv.innerHTML +="<h3>Posted @: " + new Date(ro.time).toString() + "</h3>";
+
+				$("#review-modal-scroll").append(reviewDiv);
+
+			})
+
+			$("#review-modal").modal();
+		}
+		var request = {
+			placeId: pid
+		}
+
+		var currentDiv = divsAndPlaceIds[pid];
+
+		service.getDetails(request, function(place, status) {
+			if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+				console.log(place);
+
+				if (place.hasOwnProperty("international_phone_number"))
+					currentDiv.innerHTML += ("<p class='place-div-para'>International Phone Number: " + place.international_phone_number + "</p>");
+				
+				if (place.hasOwnProperty("price_level"))
+					currentDiv.innerHTML += ("<p class='place-div-para'>Price Level: " + place.price_level + "</p>");
+				
+				if (place.hasOwnProperty("rating"))
+					currentDiv.innerHTML += ("<p class='place-div-para'>Rating: " + place.rating + "</p>");
+
+				if (place.hasOwnProperty("formatted_phone_number"))
+					currentDiv.innerHTML += ("<p class='place-div-para'>Phone Number: " + place.formatted_phone_number + "</p>");
+
+				if (place.hasOwnProperty("reviews")) {
+	
+					var reviewObjects = place.reviews;
+
+					if (reviewObjects.length > 0) {
+
+						var reviewLink = document.createElement("a");
+						reviewLink.className = "place-div-link";
+						reviewLink.innerText = "See Reviews";
+						reviewLink.onclick = function() { showReviewsDataInModal(reviewObjects, place.name) };
+
+						$(currentDiv).append(reviewLink);
+					}
+
+				}
+
+			}
+			else {
+				
+			}
+		})
+	}
+
+	function makeNewDivElement(place) {
 		var div = document.createElement("div");
-		div.id = placeObject.place_id;
-		div.innerHTML += ("<p>" + placeObject.formatted_address + "</p>");
-		div.innerHTML += ("<p>" + placeObject.name + "</p>");
+		div.id = place.place_id;
+
+		divsAndPlaceIds["divid".replace("divid", div.id)] = div;
+
+		getAdditionalInfo(div.id);
+
+		console.log(place);
+		console.log(divsAndPlaceIds[div.id]);
+
+		if (place.hasOwnProperty("formatted_address"))
+			div.innerHTML += ("<p class='place-div-para'> Address: " + place.formatted_address + "</p>");
+
+		if (place.hasOwnProperty("name"))
+			div.innerHTML += ("<p class='place-div-para'> Name: " + place.name + "</p>");
+
+		if (place.hasOwnProperty("opening_hours")) {
+			var openHoursObject = place.opening_hours;
+			if (openHoursObject.hasOwnProperty("open_now"))
+				div.innerHTML += ("<p class='place-div-para'> Open Now: " + openHoursObject.open_now + "</p>");
+		}
+
 
 		return div;
 	}
-
-	function resetMarkers() {
-		console.log("markers are currently ");
-		console.log(markers);
-		console.log("resetting markers");
-		for (var i = 0, marker; marker=markers[i]; i++) {
-			marker.setMap(null);
-		}
+	function addPlaceToView(place) {
+		var newPlaceDiv = makeNewDivElement(place);
+		$("#places-box").append(newPlaceDiv);
 	}
-	function handlePlace(place) {
-		console.log(place);
-		var image = {
-			url: place.icon,
-			size: new google.maps.Size(71, 71),
-			origin: new google.maps.Point(0, 0),
-			anchor: new google.maps.Point(17, 34),
-			scaledSize: new google.maps.Size(25, 25)
-		};
 
+	function createMarker(place) {
 		var marker = new google.maps.Marker({
 			map: map,
 			title: place.name,
 			position: place.geometry.location
 		});
-		
-		var newPlaceDiv = makeNewDivElement(place);
-		$("#places-box").append(newPlaceDiv);
 
 		markers.push(marker);
-		bounds.extend(place.geometry.location);
+
+		google.maps.event.addListener(marker, 'click', function() {
+	    	infowindow.setContent(place.name);
+	    	infowindow.open(map, this);
+  		});
 	}
 	function resetPlaces() {
 		currentPlaces = [];
 		$("#places-box").empty();
+	}
+
+	function handlePlace(place) {
+		
+		createMarker(place);
+		addPlaceToView(place);
+		bounds.extend(place.geometry.location);
+	}
+
+	function resetMarkers() {
+		for (var i = 0, marker; marker=markers[i]; i++) {
+			marker.setMap(null);
+		}
+		markers = [];
+	}
+	function showNumberOfResults(numResults) {
+		if (numResults == 1)
+			$("#number-of-results").html("There is x Result".replace('x', numResults));
+		else
+			$("#number-of-results").html("There are x Results".replace('x', numResults));
 	}
 	function searchFunction() {
 		var places = googleSearchBox.getPlaces();
 		if (places.length == 0)
 			return
 
+		showNumberOfResults(places.length);
 		resetPlaces();
 		resetMarkers();
-		markers = [];
 
 		bounds = new google.maps.LatLngBounds();
 		for (var i = 0, place; place=places[i]; i++) {
@@ -63,29 +165,48 @@ function initMap() {
 		}
 		map.fitBounds(bounds);
 	}
-
-	var mapOptions = {
-					
+	function setBounds() {
+		bounds = new google.maps.LatLngBounds(
+			new google.maps.LatLng(-33.8902, 151.1759),
+			new google.maps.LatLng(-33.8474, 151.2631)
+		)
+		map.fitBounds(bounds);
+	}
+	function createMapOptions() {
+		mapOptions = {};
+	}
+	function setupGoogleSearch() {
+		var inputSearch = document.getElementById("search");
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputSearch);
+		googleSearchBox = new google.maps.places.SearchBox(inputSearch);		
+	}
+	function createMap() {
+		map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		service = new google.maps.places.PlacesService(map);
+	}
+	function boundsChangedCallBack() {
+		bounds = map.getBounds();
+		googleSearchBox.setBounds(bounds);
+	}
+	function tilesLoadedCallBack() {
+		if (document.getElementById("search").style.display != "")
+			$("#search").show();
+	}
+	function addMapListeners() {
+		google.maps.event.addListener(map, 'bounds_changed', boundsChangedCallBack);
+		google.maps.event.addListener(map, 'tilesloaded', tilesLoadedCallBack);
+		google.maps.event.addListener(googleSearchBox, 'places_changed', searchFunction);
 	}
 
-	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-	bounds = new google.maps.LatLngBounds(
-		new google.maps.LatLng(-33.8902, 151.1759),
-		new google.maps.LatLng(-33.8474, 151.2631)
-	)
-	map.fitBounds(bounds);
-
-	var inputSearch = document.getElementById("search");
-	map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputSearch);
-	var googleSearchBox = new google.maps.places.SearchBox(inputSearch);
-				
-	google.maps.event.addListener(googleSearchBox, 'places_changed', searchFunction);
-	google.maps.event.addListener(map, 'bounds_changed', function() {
-		var bounds = map.getBounds();
-		googleSearchBox.setBounds(bounds);
-	})
-
-
+	createMapOptions();
+	createMap();
+	setupGoogleSearch();
+	addMapListeners();
+	setBounds();
+	
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+
+addMapLoadListener();
+
+
 
